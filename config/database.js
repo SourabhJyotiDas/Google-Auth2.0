2e20665c6dc3dc52b92e651ea4ab5c2337c45e44
Sourabh Jyoti Das <dassourabh126@gmail.com>
2024-01-04 20:10:46 +0530

    sadsadsa

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
    try {
-      const { connection } = await mongoose.connect("mongodb://localhost:27017/mbaBurgerWalaNEw");
+      const { connection } = await mongoose.connect("mongodb+srv://nextjs13:nextjs13@inotebook.ukflwtj.mongodb.net/GoogleLogin");
       console.log(`Database is connect with ${connection.host}`);
    } catch (error) {
       console.log(error);
